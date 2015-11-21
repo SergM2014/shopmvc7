ce2303866a5a432773ab42cc01e86d2c4f9ca0a2
SergM2014 <weisse011@gmail.com>
2015-11-21 21:36:50 +0200

    технический коммит2

--- protected/views/contacts.php
+++ protected/views/contacts.php
@@ -22,11 +22,11 @@
                           value="<?php if(isset($_POST['name'])) echo $_POST['name']; ?>" required ></p>
 
                 <label for="phone" >Телефон<span class="red">*</span></label>   <small class="red"><?php if(isset($error['phone'])) echo $error['phone']; ?></small>
-                <p><input type="text" name="phone" id="phone" placeholder="Введите Ваш телефон" <?php if(isset($error['phone'])) echo 'class="error"'; ?>
+                <p><input type="tel" name="phone" id="phone" placeholder="Введите Ваш телефон" <?php if(isset($error['phone'])) echo 'class="error"'; ?>
                           value="<?php if(isset($_POST['phone'])) echo $_POST['phone']; ?>" required ></p>
 
                 <label for="email">Email<span class="red">*</span></label>   <small class="red"><?php if(isset($error['email'])) echo $error['email']; ?></small>
-                <p><input type="text" name="email" id="email" placeholder="Введите Ваш почтовый ящик" <?php if(isset($error['email'])) echo 'class="error"'; ?>
+                <p><input type="email" name="email" id="email" placeholder="Введите Ваш почтовый ящик" <?php if(isset($error['email'])) echo 'class="error"'; ?>
                           value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>" required ></p>
 
                 <label for="message">Ваше Сообщение<span class="red">*</span></label>   <small class="red"><?php if(isset($error['message'])) echo $error['message']; ?></small>
